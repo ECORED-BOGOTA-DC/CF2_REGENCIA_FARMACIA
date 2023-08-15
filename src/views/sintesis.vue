0406fdcb590fbc3327ee1da4f191598f6e140942
jorozcop <maechavarriaor@gmail.com>
2023-08-15 17:24:35 -0500

    ajustes
    ajustes

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -9,7 +9,7 @@
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="Muestra síntesis del componente formativo, el diagrama de actividades se termina la descripción de las tres técnicas usadas para la especificación y el análisis de requisitos de sofware, historias de usuario y diagramada de actividades. Como se pudo analizar cada una de las técnicas nos brinda características para lograr representar gráficamente y de una forma clara, los requisitos que un usuario necesita poder cumplir satisfactoriamente con las solicitudes de los clientes y usuarios, por otro lado se pueden usar todas las técnicas.")
       .col-auto
         a.anexo.mb-4(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
